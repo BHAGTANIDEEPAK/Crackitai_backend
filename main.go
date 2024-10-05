2f96ad3a760fe6c726a4d3d02f3ea13596422774
BHAGTANIDEEPAK <bhagatnideepak77@gmail.com>
2024-10-05 22:24:18 +0530

    added api endpoints for get interview history and post interview

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Interview struct {
+	Email      string `json:"Email"`
+	Position   string `json:"Position"`
+	Techstack  string `json:"Techstack"`
+	Experience int    `json:"Experience"`
+}
+
+var Interviews = []Interview{
+	{Email: "deepak@gmail.com", Position: "Front End Developer", Techstack: "Javascript", Experience: 1},
+	{Email: "deepak@gmail.com", Position: "Backend End Developer", Techstack: "Python", Experience: 1},
+	{Email: "ashutosh@gmail.com", Position: "Full Stack Developer", Techstack: "Javascript, Nodejs, cloud", Experience: 1},
+}
+
+func getInterviews(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, Interviews)
+}
+
+func addInterview(context *gin.Context) {
+	var newInterview Interview
+
+	if err := context.BindJSON(&newInterview); err != nil {
+		return
+	}
+
+	Interviews = append(Interviews, newInterview)
+
+	context.IndentedJSON(http.StatusCreated, Interviews)
+}
+
+func getInterviewByEmail(email string) ([]*Interview, error) {
+	var matchingInterviews []*Interview
+
+	for i, t := range Interviews {
+		if t.Email == email {
+			matchingInterviews = append(matchingInterviews, &Interviews[i])
+		}
+	}
+
+	if len(matchingInterviews) == 0 {
+		return nil, errors.New("Interview history not found")
+	}
+
+	return matchingInterviews, nil
+
+}
+
+func getInterview(context *gin.Context) {
+	email := context.Param("email")
+	interview, error := getInterviewByEmail(email)
+
+	if error != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Interview history not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, interview)
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/getInterviews", getInterviews)
+	router.GET("/getInterviews/:email", getInterview)
+	router.POST("/addInterview", addInterview)
+	router.Run("localhost:9090")
+}
